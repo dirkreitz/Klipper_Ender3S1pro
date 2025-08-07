a1ef61556e2b91f28f748424f626bd46fdd1767b
dirkreitz <56075427+dirkreitz@users.noreply.github.com>
2025-08-07 11:06:14 +0200

    Update git_backup.sh

--- scripts/git_backup.sh
+++ scripts/git_backup.sh
@@ -63,7 +63,7 @@ push_config(){
     current_date=$(date +"%Y-%m-%d %T")
     git commit -m "Backup triggered on $current_date" -m "$m1" -m "$m2" -m "$m3" -m "$m4"
     #  git push "https://rootiest:$GH_TOKEN@github.com/rootiest/zippy-klipper_config.git"
-    git push "git@github.com:dirkreitz/Klipper_config.git"
+    git push -u origin main
 }
 
 grab_version
